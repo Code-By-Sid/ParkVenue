a59a01750cf247139c35d091053f21d5dfb80ce5
Code-By-Sid <siddhesh.more.dev@gmail.com>
2026-09-02 14:19:24 +0530

    Remove unused templates, views, and utility functions related to reservations and user management; delete associated tests and URL patterns to streamline the application.

--- acc/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser,ParkingSlot,UserParkingBookmark,Reservation
-# Register your models here.
-
-admin.site.register(CustomUser)
-admin.site.register(ParkingSlot)
-admin.site.register(UserParkingBookmark)
-admin.site.register(Reservation)
